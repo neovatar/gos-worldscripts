3035eb30acbba756cc067a5903a9af4c84b43b14
Thomas Seliger <neovatar@gmail.com>
2024-02-06 20:28:43 +0100

    Test another world script

--- scripts/worldscripts.js
+++ scripts/worldscripts.js
@@ -1,11 +1,6 @@
-// This script will change the default token configuration for newly created or imported actors
-//   (does not affect actors already in the world).
-// It changes the following:
-// - Always show all resource bars for owners of the token
-Hooks.on("preCreateActor", (actorData, options, userId) => {
-  actorData.token = actorData.token ?? {};
-  mergeObject(actorData.token, {
-    displayName: CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,
-    displayBars: CONST.TOKEN_DISPLAY_MODES.OWNER,
-  });
+Hooks.once("init", () => {
+  CONFIG.DND5E.languages.coolspeech = "Cool Speech";
+  CONFIG.DND5E.languages.standard.children.supercommon = "Super Common";
+  CONFIG.DND5E.languages.exotic.children.uncommon = "Uncommon";
 });
+
