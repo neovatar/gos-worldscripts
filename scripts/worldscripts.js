9d088e3d63bbd8b9a46a3f909180e2c7ae32477c
Thomas Seliger <neovatar@gmail.com>
2024-02-07 16:47:19 +0100

    Add total prepared spell to charsheet

--- scripts/worldscripts.js
+++ scripts/worldscripts.js
@@ -15,3 +15,24 @@ Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
     const removeUnid = buttons.findIndex(option => option.name === 'DND5E.Identify');
     buttons.findSplice(e => e.name === "DND5E.Identify");
 });
+
+
+// Add total prepared spells to the spell headers on the character sheet
+Hooks.on("renderActorSheet5eCharacter2", function(app, [html]) {
+  const prep = app.document.items.reduce((acc, i) => {
+    if (i.type === 'spell' && i.system.preparation.mode === "prepared" && i.system.preparation.prepared & i.system.level !== 0) {
+      acc.push(i.name);
+    }
+    return acc;
+  }, []);
+  const headers = html.querySelectorAll('div[data-type="spell"]');
+  for (const header of headers) {
+    const type = header.querySelector('h3[class="item-name spell-header"]').textContent;
+    if (!type.includes('Innate') && !type.includes('At-Will') && !type.includes('Cantrip') && !type.includes('Pact')) {
+      const controls = header.querySelector('div[class="item-header item-controls"]');
+      controls.textContent += `${prep.length}`;
+      controls.setAttribute("data-tooltip", "Total Prepared Spells");
+      controls.setAttribute("style", "color: white;");
+    };
+  };
+});
