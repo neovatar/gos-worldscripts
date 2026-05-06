b67d11c9f1c8e4d90180fcbb2dda5d68e5348403
Thomas Seliger <neovatar@gmail.com>
2026-05-06 15:30:51 +0200

    fix: remove identify button for players

--- scripts/worldscripts.js
+++ scripts/worldscripts.js
@@ -1,11 +1,10 @@
 // Prevent Players from Identifying their items by hiding the buttons from them, only the GM will be able to Identify them
 // Remove Identify button at top of Item Sheet
-Hooks.on("renderItemSheet", (sheet, [html]) => {
-  console.log("Prevent identify | rendering item sheet");
+Hooks.on("renderItemSheet5e", (sheet, [html]) => {
   if (game.user.isGM) return;
   const unidentified = sheet.item.system.identified === false;
   if (!unidentified) return;
-  html.querySelectorAll(".header-control.pseudo-header-control.state-toggle.fa-solid.icon.fa-wand-sparkles.toggle-identified").forEach(n => n.remove());
+  html.parentElement.querySelectorAll('[data-action="toggleState"][data-property="system.identified"]').forEach(n => n.remove());
 });
 
 // Remove Identify button from Item Context menu on Actor Sheet
@@ -16,23 +15,5 @@ Hooks.on("dnd5e.getItemContextOptions", (item, buttons) => {
   const removeUnid = buttons.findIndex(option => option.name === 'DND5E.Identify');
   buttons.findSplice(e => e.name === "DND5E.Identify");
 });
-
-// Add total prepared spells to the spell headers on the character sheet
-Hooks.on("renderActorSheet5eCharacter2", function(app, [html]) {
-  const prep = app.document.items.reduce((acc, i) => {
-    if (i.type === 'spell' && i.system.preparation.mode === "prepared" && i.system.preparation.prepared & i.system.level !== 0) {
-      acc.push(i.name);
-    }
-    return acc;
-  }, []);
-  const headers = html.querySelectorAll('div[data-type="spell"]');
-  for (const header of headers) {
-    const type = header.querySelector('h3[class="item-name spell-header"]').textContent;
-    if (!type.includes('Innate') && !type.includes('At-Will') && !type.includes('Cantrip') && !type.includes('Pact')) {
-      const controls = header.querySelector('div[class="item-header item-controls"]');
-      controls.textContent += `${prep.length}`;
-      controls.setAttribute("data-tooltip", "Total Prepared Spells");
-      controls.setAttribute("style", "color: white; align-items: center;");
-    };
-  };
-});
+<button type="button" class="header-control pseudo-header-control state-toggle fa-solid icon fa-wand-sparkles toggle-identified" 
+data-action="toggleState" data-property="system.identified" data-tooltip-direction="DOWN" aria-label="Unidentified" data-tooltip="DND5E.Unidentified.Title"></button>
